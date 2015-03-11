3d3620ef4c1c4e5d5766b7bc8c0b7c35d427b1f3
Min Guo <xmguomin@gmail.com>
2015-03-10 22:16:26 -0400

    Scale works

--- computer-graphics/assignment5/martix.js
+++ computer-graphics/assignment5/martix.js
@@ -8,7 +8,7 @@ function scaleMatrix(x) {
   return transformMatrix;
 }
 
-function  newPoint (matrix, originPoint)  {
+function  generatePoint (matrix, originPoint)  {
   var Point = [0, 0, 0, 1];
   for (i = 0; i < 4; i ++) {
     for (j = 0; j < 4; j ++){
