7d793f2f92513901e5e933c77a1782ddf38e9193
Min Guo <xmguomin@gmail.com>
2015-03-10 21:12:15 -0400

    Add assignemnt5

---
+++ computer-graphics/assignment5/martix.js
@@ -0,0 +1,19 @@
+
+
+function scaleMatrix(x) {
+  var transformMatrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]];
+  for ( i = 0; i < 2; i ++) {
+    transformMatrix[i][i] = x;
+  }
+  return transformMatrix;
+}
+
+function  newPoint (matrix, originPoint)  {
+  var Point = [0, 0, 0, 1];
+  for (i = 0; i < 4; i ++) {
+    for (j = 0; j < 4; j ++){
+          Point[i] = Point[i] + matrix[i][j] * originPoint[j];
+      }
+  }
+  return Point;
+}
