2ef7d10da25bdbab85a8cde01461c12ed6858cd6
Min Guo <xmguomin@gmail.com>
2015-03-10 23:26:19 -0400

    Translate works

--- computer-graphics/assignment5/martix.js
+++ computer-graphics/assignment5/martix.js
@@ -1,10 +1,17 @@
 
 
-function scaleMatrix(x) {
+function scaleMatrix(x, y, w) {
   var transformMatrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]];
   for ( i = 0; i < 2; i ++) {
     transformMatrix[i][i] = x;
   }
+  transformMatrix[0][1] = y;
+  transformMatrix[1][0] = y;
+
+  for (i = 0; i < 3; i ++) {
+    transformMatrix[i][3] = w;
+  }
+
   return transformMatrix;
 }
 
