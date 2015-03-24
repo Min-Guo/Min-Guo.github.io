806f5ca7cbcab0a8563db08f86e6a6f8b203acce
Min Guo <xmguomin@gmail.com>
2015-03-24 10:29:24 -0400

    Add perspective

--- computer-graphics/assignment5/martix.js
+++ computer-graphics/assignment5/martix.js
@@ -18,12 +18,12 @@ Matrix.prototype.identity = function (x) {
 }
 
 
-Matrix.prototype.perspective = function (x, y) {
+Matrix.prototype.perspective = function (x, z) {
   var transformMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
-  for ( i = 0; i < 4; i ++) {
-    transformMatrix[i][i] = x / y;
+  for ( i = 0; i < 2; i ++) {
+    transformMatrix[i][i] = x / z;
   }
-  transformMatrix[2][2] = x / y / y;
+  transformMatrix[2][2] = x / z ;
   return transformMatrix;
 }
 
@@ -39,7 +39,7 @@ Matrix.prototype.translate = function (x) {
   return transformMatrix;
 }
 
-Matrix.prototype.rotate = function (x) {
+Matrix.prototype.rotateZ = function (x) {
   var transformMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
   for (i = 0; i <2; i ++){
     transformMatrix[i][i] = Math.cos(x * Math.PI / 180);
@@ -50,10 +50,47 @@ Matrix.prototype.rotate = function (x) {
   return transformMatrix;
 }
 
+Matrix.prototype.rotateX = function (x) {
+  var transformMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+  transformMatrix[0][0] = transformMatrix[3][3] = 1;
+  transformMatrix[1][1] = transformMatrix[2][2] = Math.cos(x * Math.PI / 180);
+  transformMatrix[1][2] = Math.sin(x * Math.PI / 180);
+  transformMatrix[2][1] = - Math.sin(x * Math.PI / 180);
+
+  return transformMatrix;
+}
+
+Matrix.prototype.rotateY = function (x) {
+  var transformMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+  transformMatrix[0][0] = transformMatrix[2][2] = Math.cos(x * Math.PI / 180);
+  transformMatrix[0][2] = Math.sin(x * Math.PI / 180);
+  transformMatrix[2][0] = - Math.sin(x * Math.PI / 180);
+
+  return transformMatrix;
+}
+
+Matrix.prototype.translateZ = function (x) {
+  var transformMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+    transformMatrix[2][3] = x;
+  return transformMatrix;
+}
+
+Matrix.prototype.matrixMultiply = function (matrix1, matrix2) {
+  var matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]];
+  for (var i = 0; i < 4; i ++) {
+    for (var j = 0; j < 4; j ++) {
+      for (var k = 0; k < 4; k++) {
+        matrix[i][j] = matrix[i][j] + matrix1[i][k] * matrix2[k][j];
+      }
+    }
+  }
+  return matrix;
+}
+
 function  generatePoint (matrix, originPoint)  {
   var Point = [0, 0, 0, 1];
-  for (i = 0; i < 4; i ++) {
-    for (j = 0; j < 4; j ++){
+  for (var i = 0; i < 4; i ++) {
+    for (var j = 0; j < 4; j ++){
           Point[i] = Point[i] + matrix[i][j] * originPoint[j];
       }
   }
